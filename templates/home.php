2758eeb7f4976c339567149dd0a797d633050634
ravikishore1993 <ravikishore1993@gmail.com>
2014-11-27 16:40:02 +0530

    Adds more style :metal:

--- templates/home.php
+++ templates/home.php
@@ -2,10 +2,22 @@
 <html>
 <head>
 	<title>Shorten It!</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="shortcut icon" href="assets/icons/favicon.ico"/>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
-
+	<div id="main-title">
+		Shorten It!	
+	</div>
+	<div id="subtitle">
+		A Minimal URL shortener
+	</div>
+	<div id="form">
+		<input type="text" id="urlinput" />
+		<br>
+		<button id="urlbutton">Go</button>
+	</div>
 </body>
 </html>
