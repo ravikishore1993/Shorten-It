a7e9782301699c90b4821dd395029ec1d731b6d0
ravikishore1993 <ravikishore1993@gmail.com>
2014-11-28 01:53:22 +0530

    404 page

---
+++ templates/404.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shorten It!</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="shortcut icon" href="assets/icons/favicon.ico"/>
+</head>
+<body>
+	<div id="error">
+		404
+	</div>
+</body>
+</html>
